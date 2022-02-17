3cffa5379d9e0945b6da98d90db2231adcf008ac
Ian Jennings <ian@meetjennings.com>
2022-02-17 13:52:46 -0600

    this doesn't change much...

--- js/game_manager.js
+++ js/game_manager.js
@@ -193,7 +193,7 @@ GameManager.prototype.getVector = function (direction) {
   // Vectors representing tile movement
   var map = {
     0: { x: 0, y: -1 }, // Up
-    1: { x: 1, y: 0 }, // Right
+    1: { x: 0, y: 0 }, // Right
     2: { x: 0, y: 1 }, // Down
     3: { x: -1, y: 0 }, // Left
   };
